cc593becf5361f99f850eac173937d0b0a236a24
Sahir Ul Hassan <sahirulhasan2002@gmail.com>
2024-12-14 11:23:19 +0000

    Updated Singly-Linked Lists.

--- src/linked_list/SinglyLinkedList.java
+++ src/linked_list/SinglyLinkedList.java
@@ -1,41 +1,143 @@
 package linked_list;
 
 public class SinglyLinkedList {
-    Node head;
+    private SinglyLinkedNode head;
 
     public SinglyLinkedList() {
-        this.head = null;
+        head = null;
     }
 
-    public void insert(int pos, int element) {
-        if (pos < 1) {
-            return;
-        }
-        Node newNode = new Node(element);
+    public SinglyLinkedNode getHead() {
+        return head;
+    }
 
-        if (pos == 1) {
-            newNode.next = head;
+    public void setHead(SinglyLinkedNode head) {
+        this.head = head;
+    }
+
+    public void append(int data) {
+        SinglyLinkedNode newNode = new SinglyLinkedNode(data);
+        if (isEmpty()) {
             head = newNode;
         } else {
-            Node currentNode = head;
-            int currentPos = 1;
-            while (currentNode.next != null) {
-                if (currentPos == pos) {
-                    break;
-                }
-                currentPos++;
-                currentNode = currentNode.next;
+            SinglyLinkedNode current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
             }
-            newNode.next = currentNode.next;
-            currentNode.next = newNode;
+            current.setNext(newNode);
         }
     }
 
+    public void prepend(int data) {
+        SinglyLinkedNode newNode = new SinglyLinkedNode(data);
+        newNode.setNext(head);
+        head = newNode;
+    }
+
+    public void insert(int pos, int data) {
+        if (pos < 0) {
+            throw new IllegalArgumentException("Enter a valid position to insert a value.");
+        } else if (pos == 0) {
+            prepend(data);
+        } else {
+            SinglyLinkedNode newNode = new SinglyLinkedNode(data);
+            SinglyLinkedNode current = head;
+            for (int i = 0; i < pos - 1; i++) {
+                current = current.getNext();
+            }
+
+            if (current == null) {
+                throw new IllegalArgumentException("Position is out of bounds.");
+            }
+
+
+            newNode.setNext(current.getNext());
+            current.setNext(newNode);
+        }
+    }
+
+    public void deleteByValue(int target) {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("List is empty.");
+        }
+        if (head.getData() == target) {
+            deleteHead();
+        } else {
+            SinglyLinkedNode current = head;
+            while (current.getNext() != null && current.getNext().getData() != target) {
+                current = current.getNext();
+            }
+            if (current.getNext() == null) {
+                throw new IllegalArgumentException("Value not found.");
+            }
+            current.setNext(current.getNext().getNext());
+        }
+    }
+
+    public void deleteHead() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("List is empty.");
+        }
+        head = head.getNext();
+    }
+
+    public void deleteTail() {
+        SinglyLinkedNode current = head;
+        while (current.getNext().getNext() != null) {
+            current = current.getNext();
+        }
+        current.setNext(null);
+    }
+
+    public void delete(int pos) {
+        if (pos < 0) {
+            throw new IllegalArgumentException("Enter a valid position to delete a value.");
+        } else if (pos == 0) {
+            deleteHead();
+        } else {
+            SinglyLinkedNode current = head;
+            for (int i = 0; i < pos-1; i++) {
+                current = current.getNext();
+            }
+            if (current == null) {
+                throw new IllegalArgumentException("Position is out of bounds.");
+            }
+            current.setNext(current.getNext().getNext());
+        }
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder temp = new StringBuilder();
+        SinglyLinkedNode current = head;
+        while (current != null) {
+            temp.append(current.getData()).append(" ");
+            current = current.getNext();
+        }
+        return temp.toString();
+    }
+
+
     public static void main(String[] args) {
-        SinglyLinkedList linkedList = new SinglyLinkedList();
-        linkedList.insert(1, 10);
-        linkedList.insert(2,20);
-        linkedList.insert(3, 30);
-        linkedList.insert(1, 5);
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        System.out.println(list);
+        list.prepend(0);
+        System.out.println(list);
+        list.insert(0,0);
+        System.out.println(list);
+        list.deleteTail();
+        System.out.println(list);
+        list.delete(1);
+        System.out.println(list);
+        list.deleteByValue(1);
+        System.out.println(list);
     }
 }
---
+++ src/linked_list/SinglyLinkedNode.java
@@ -0,0 +1,27 @@
+package linked_list;
+
+public class SinglyLinkedNode {
+    private int data;
+    private SinglyLinkedNode next;
+
+    public SinglyLinkedNode(int data) {
+        this.data = data;
+        next = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public SinglyLinkedNode getNext() {
+        return next;
+    }
+
+    public void setNext(SinglyLinkedNode next) {
+        this.next = next;
+    }
+}
